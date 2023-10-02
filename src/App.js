197ff9067680028cb61942f50ac19d34b3c5dd31
Sreel <sreelakshmyaj@gmail.com>
2023-10-02 14:19:10 +0530

    Setup Firebase and React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <div className='app'>
+        <Routes>
+          <Route path='/' element={<h1>Home Page</h1>} />
+          <Route path='/checkout' element={<h1>Checkout</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
