e3cb836777a92fdd2cbebcbed141a49d5f999c78
Sreel <sreelakshmyaj@gmail.com>
2023-10-13 23:09:55 +0530

    Created login form

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,18 +4,23 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='login__container'>
+    <div className='login'>
         <div className='login__logo'>
             <Link to="/">
                 <img src={logo} alt='logo' width={100}/>
             </Link>
         </div>
         <div className='login__form'>
-            <p>Sign in</p>
-            <p style={{fontSize: '13px'}}><strong>Email or mobile phone number</strong></p>
-            <input className='login__formInput' type='text' />
-            <button className='btn login__formBtn'>Continue</button>
-            <p><small>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice</small></p>
+            <p className='login__formHeader'>Sign in</p>
+            <form>
+              <p style={{fontSize: '13px'}}><strong>Email or mobile phone number</strong></p>
+              <input className='login__formInput' type='text' />
+
+              <p style={{fontSize: '13px'}}><strong>Password</strong></p>
+              <input className='login__formInput' type="password" />
+              <button className='btn login__formBtn'>Sign in</button>
+              <p><small>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice</small></p>
+            </form>
         </div>
     </div>
   )
